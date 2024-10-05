cc6db299b5fd45347d173ebce51580f02a0fe310
sherif <sherifahmedelsayed2@gmail.com>
2024-10-05 10:59:00 +0300

    Add Project Folders

--- android/app/src/main/kotlin/com/exapps/spacex/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.exapps.spacex
-
-import io.flutter.embedding.android.FlutterActivity
-import com.learntoflutter.flutter_embed_unity_android.unity.FakeUnityPlayerActivity
-
-class MainActivity: FakeUnityPlayerActivity ()
